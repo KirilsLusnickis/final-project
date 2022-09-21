c0421bdf2c9b8ceec504198dc549f8d1656ada5c
KirilsLusnickis <lusnickis2@inbox.lv>
2022-09-21 18:00:03 +0300

    add styles to nav

--- FRONTEND/src/Components/topBar/Button.js
+++ FRONTEND/src/Components/topBar/Button.js
@@ -1,33 +1,33 @@
-import React from "react";
-import "./button.css";
-import { Link } from "react-router-dom";
+// import React from "react";
+// import "./button.css";
+// import { Link } from "react-router-dom";
 
-const STYLES = ["btn-initial", "btn-contour"];
+// const STYLES = ["btn-initial", "btn-contour"];
 
-const SIZES = ["btn-small", "btn-big"];
+// const SIZES = ["btn-small", "btn-big"];
 
-export const Button = ({
-  children,
-  onClick,
-  type,
-  buttonStyle,
-  buttonSize,
-}) => {
-  const checkButtonStyle = STYLES.includes(buttonStyle)
-    ? buttonStyle
-    : STYLES[0];
+// export const Button = ({
+//   children,
+//   onClick,
+//   type,
+//   buttonStyle,
+//   buttonSize,
+// }) => {
+//   const checkButtonStyle = STYLES.includes(buttonStyle)
+//     ? buttonStyle
+//     : STYLES[0];
 
-  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+//   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-  return (
-    <Link to="/sign-up" className="btn-mobile">
-      <Button
-        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-        onClick={onClick}
-        type={type}
-      >
-        {children}
-      </Button>
-    </Link>
-  );
-};
+//   return (
+//     <Link to="/sign-up" className="btn-mobile">
+//       <Button
+//         className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+//         onClick={onClick}
+//         type={type}
+//       >
+//         {children}
+//       </Button>
+//     </Link>
+//   );
+// };
--- FRONTEND/src/Components/topBar/TopBar.js
+++ FRONTEND/src/Components/topBar/TopBar.js
@@ -1,31 +1,19 @@
 import "./topBar.css";
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
-import { Button } from "./Button";
 
 const TopBar = () => {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
 
   const manualClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if (window.innerWidth <= 950) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-
-  window.addEventListener("resize", showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            rter <i class="fa-solid fa-cloud" />
+            WEATHER <i class="fa-solid fa-cloud" />
           </Link>
           <div className="menu-icon" onClick={manualClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
@@ -50,7 +38,7 @@ const TopBar = () => {
                 Chat
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item sign">
               <Link
                 to="/sign-up"
                 className="nav-links"
@@ -60,7 +48,6 @@ const TopBar = () => {
               </Link>
             </li>
           </ul>
-          {button && <Button buttonStyle="btn-contour">SIGN UP</Button>}
         </div>
       </nav>
     </>
