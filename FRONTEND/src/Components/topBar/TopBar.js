36f3becef48caab48deb6f9100260989df44f8f2
KirilsLusnickis <lusnickis2@inbox.lv>
2022-09-21 14:16:05 +0300

    add link to navbar

--- FRONTEND/src/Components/topBar/TopBar.js
+++ FRONTEND/src/Components/topBar/TopBar.js
@@ -6,7 +6,9 @@ const TopBar = () => {
   const [click, setClick] = useState(false);
 
   const manualClick = () => setClick(!click);
-  
+
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
     <nav className='navbar'>
@@ -17,6 +19,28 @@ const TopBar = () => {
           <div className='menu-icon' onClick={manualClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
           </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/weather' className='nav-links' onClick={closeMobileMenu}>
+                Weather
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/chat' className='nav-links' onClick={closeMobileMenu}>
+                Chat
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/sign-up' className='nav-links' onClick={closeMobileMenu}>
+                Sign-up
+              </Link>
+            </li>
+          </ul>
         </div>
     </nav>
     </>
